4b012ca4212b651e4896217c7e626c01934bf10a
Kalle Pronk <kallepronk@proton.me>
2026-04-05 16:39:17 +0200

    add plugins

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = '<Tab>',
+          dismiss = '<C-]>',
+          next = '<M-]>',
+          prev = '<M-[>',
+        },
+      },
+      panel = { enabled = false },
+    }
+  end,
+}
